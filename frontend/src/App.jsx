6005a3526d40637fd9acb7fc6812de53f6f992e3
Vishal Singh <singh287686@gmail.com>
2025-04-06 07:22:47 +0530

    added all the codes till now

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header.jsx'
+import ServicesPage from './components/ServicesPage.jsx'
+import CaseAnalysisPage from './components/CaseAnalysisPage.jsx'
+import DocumentGenerator from './components/DocumentGenerator.jsx'
+import CaseOutcomePrediction from './components/CaseOutcomePrediction.jsx'
+
+const App = () => {
+  return (
+    <Router>
+      <div className=''>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/generate" element={<DocumentGenerator />} />
+          <Route path="/compute" element={<CaseAnalysisPage />} />
+          <Route path="/outcome" element={<CaseOutcomePrediction />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
